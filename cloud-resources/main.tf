d1f50461349a129fee3c2d1e9f99c5b9f9d5d2e0
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-05 10:02:54 +0000

    Adds pipeline lambda role, with basic logging permissions

--- cloud-resources/main.tf
+++ cloud-resources/main.tf
@@ -7,3 +7,42 @@ resource "aws_ecr_repository" "pipeline_ecr" {
   image_tag_mutability = "MUTABLE"
   force_delete = true
 }
+
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "pipeline_lambda_permissions" {
+  statement {
+    effect = "Allow"
+    
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "pipeline_lambda" {
+  name = "pigasus-pipeline-lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+}
+
+resource "aws_iam_role_policy" "pipeline_lambda" {
+  name = "pigasus-pipeline-lambda"
+  role = aws_iam_role.pipeline_lambda.id
+  policy = data.aws_iam_policy_document.pipeline_lambda_permissions.json
+}

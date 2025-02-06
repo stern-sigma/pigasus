a3a05053bdc3d8e906c13ebd64beab626b8d6fee
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-06 10:57:14 +0000

    Adds s3 bucket terraform to archive

--- cloud-resources/outputs.tf
+++ cloud-resources/outputs.tf
@@ -5,3 +5,7 @@ output "pipeline_ecr_url" {
 output "archive_ecr_url" {
   value = aws_ecr_repository.archive_ecr.repository_url
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.archive.id
+}

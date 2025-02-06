8fb5acbd3ea913b732405f732f1a9514925f1308
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-06 17:09:18 +0000

    Finishes dashboard security group

--- cloud-resources/streamlit-server.tf
+++ cloud-resources/streamlit-server.tf
@@ -16,3 +16,28 @@ resource "null_resource" "initialise_dashboard_ecr" {
 
   depends_on = [aws_ecr_repository.dashboard_ecr]
 }
+
+resource "aws_security_group" "dashboard" {
+  name = "pigasus-dashboard"
+  vpc_id = data.aws_vpc.c15-vpc.id 
+
+  dynamic "ingress" {
+    for_each = var.dashboard_sg_ports
+
+    content {
+      from_port = ingress.value
+      to_port = ingress.value 
+      protocol = "tcp"
+    }
+  }
+
+  dynamic "egress"{
+    for_each = var.dashboard_sg_ports
+
+    content {
+      from_port = egress.value 
+      to_port = egress.value 
+      protocol = "tcp"
+    }
+  }
+}

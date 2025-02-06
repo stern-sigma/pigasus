c1a73fcc9cc5db053938af2c0a922e8e9cbffd21
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2025-02-06 16:27:16 +0000

    Minor name changes

--- cloud-resources/streamlit-server.tf
+++ cloud-resources/streamlit-server.tf
@@ -8,8 +8,8 @@ resource "null_resource" "initialise_dashboard_ecr" {
   provisioner "local-exec" {
     command = <<EOT
       aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.eu-west-2.amazonaws.com
-      docker build --platform linux/arm64 --provenance false -t pigasus-pipeline .
-      docker tag pigasus-pipeline:latest ${aws_ecr_repository.dashboard_ecr.repository_url}:latest
+      docker build --platform linux/arm64 --provenance false -t pigasus-dashboard .
+      docker tag pigasus-dashbaord:latest ${aws_ecr_repository.dashboard_ecr.repository_url}:latest
       docker push ${aws_ecr_repository.dashboard_ecr.repository_url}:latest    
     EOT
   }
